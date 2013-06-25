fc95d8a2864b5f3ffecf19a0925264f11ce2aad0
Jonathon Williams <jwilliams@isis.vanderbilt.edu>
2013-06-25 00:38:40 -0500

    Tweaks to init script

--- init.sh
+++ init.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-if [ ! -d ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User ]; then
-  ln -s sublime-user ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
+if [ ! -d ~/Library/Application\ Support/Sublime\ Text\ 2/Packages ]; then
+  mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 2/Packages
 else
-  echo "Looks like you already have a user directory..."
+  echo "Not creating ST package directory..."
+fi
+
+
+if [ ! -d ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User ]; then
+  ln -s $PWD/sublime-user ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
+else
+  echo "Looks like you already have an ST user directory..."
 fi
